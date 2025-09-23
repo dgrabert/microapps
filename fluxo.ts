f11478ee3bcaecae914c7bf76fec2a6f7feadaef
Igor Café <igorcafe@protonmail.com>
2025-09-23 12:34:13 -0300

    mock do ControladorFluxo

--- fluxo.ts
+++ fluxo.ts
@@ -6,18 +6,38 @@ export type Etapa = {
 
 @wrapper
 export class ControladorFluxo {
-  _etapa: string = "Etapa";
+  _etapa_atual: string = "Etapa 1";
+  _etapa_anterior: string = "Etapa anterior";
+  _etapas: Etapa[] = [
+    { nome: "Etapa 1" },
+    { nome: "Etapa 2" },
+    { nome: "Etapa anterior" },
+    { nome: "Etapa 3" },
+  ];
 
   etapa_existe(p: { nome_etapa: string }): Promise<boolean> {
-    return Promise.resolve(p.nome_etapa === this._etapa);
+    return Promise.resolve(
+      Boolean(this._etapas.find((e) => e.nome === p.nome_etapa)),
+    );
   }
 
   set_etapa(p: { nome_etapa: string }): Promise<void> {
-    this._etapa = p.nome_etapa;
+    console.log(`Fluxo: ${this._etapa_atual} -> ${p.nome_etapa}`);
+    this._etapa_anterior = this._etapa_atual;
+    this._etapa_atual = p.nome_etapa;
     return Promise.resolve();
   }
 
   get_etapa_atual(): Promise<Etapa> {
-    return Promise.resolve({ "nome": this._etapa });
+    const etapa = this._etapas.find((e) => e.nome === this._etapa_atual) ??
+      this._etapas[0];
+    console.log(`Fluxo: etapa atual: ${etapa}`);
+    return Promise.resolve(etapa);
+  }
+
+  voltar_etapa(): Promise<void> {
+    console.log(`Fluxo: ${this._etapa_atual} -> ${this._etapa_anterior}`);
+    this._etapa_atual = this._etapa_anterior;
+    return Promise.resolve();
   }
 }
