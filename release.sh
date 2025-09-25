170bf2a115cedde4c30060d319f7a608e71aa4c9
Igor Café <igorcafe@protonmail.com>
2025-09-25 14:16:02 -0300

    adicionar prints explicando oq ta sendo feito em cada passo

--- release.sh
+++ release.sh
@@ -17,17 +17,23 @@ fi
 
 tag=$1
 
+echo "> atualizando deno.json pra versao $tag"
 sed -i "s/\"version\".*/\"version\": \"$tag\",/" deno.json
 has_changes=$(git status --porcelain)
 
 if [[ $has_changes ]]
 then
+    echo "> commit do deno.json"
     git add deno.json
     git commit -m "$tag"
+else
+    echo "> deno.json ja esta na versao correta"
 fi
 
+echo "> criando tag $tag no commit atual e subindo pro remote"
 git tag $tag
 git push origin $tag
 git push
 
+echo "> publicando no JSR"
 deno publish --no-check
