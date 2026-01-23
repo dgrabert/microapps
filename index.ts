8ca86ad1c0fd8dceea11cb81035ece8aea97d2d2
Cássio Ávila <cassioavila@protonmail.com>
2026-01-23 14:24:36 -0300

    fix index

--- index.ts
+++ index.ts
@@ -6,25 +6,6 @@ export {
   postprocessing,
   preprocessing
 } from "./decorators.ts";
-import type { Etapa } from "./fluxo.ts";
-import type { Team, TemplateComponent } from "./interfaces.ts";
-import { TemplateMessage, Message, MessageContent } from "./interfaces.ts";
-import type { MensagemLLM } from "./llm.ts";
-import type { Mensagem } from "./mensagens.ts";
-import { retry } from "./retry.ts";
-import type { RespostaAgendamento, Tarefa } from "./scheduler.ts";
-
-// decorators
-export { aiFunction, exposed, moderator, postprocessing, preprocessing };
-
-// classe base
-export { MicroApp };
-
-// utils
-export { retry };
-
-// tipos e classes que o usuario pode usar
-export { TemplateMessage, Message, MessageContent };
 export type { AIParam, AiFunctionSettings } from "./decorators.ts";
 export type { Etapa } from "./fluxo.ts";
 export { TemplateMessage } from "./interfaces.ts";
@@ -33,7 +14,10 @@ export type {
   ConversationInfo,
   ConversationMeta,
   Team,
-  TemplateComponent
+  TemplateComponent,
+  TemplateMessage,
+  Message,
+  MessageContent,
 } from "./interfaces.ts";
 export type { MensagemLLM } from "./llm.ts";
 export type { Mensagem } from "./mensagens.ts";
