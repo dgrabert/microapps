5696a456498fda88e8de9b2bc14aeecb4b3f9c49
Cássio Ávila <cassioavila@protonmail.com>
2026-01-23 19:46:39 -0300

    Feat (follow up): Adds follow-up class

---
+++ follow_up.ts
@@ -0,0 +1,40 @@
+import { wrapper } from "./decorators.ts";
+
+@wrapper
+export class FollowUp {
+  quantidade_fups_enviados(): Promise<void> {
+    return Promise.resolve();
+  }
+
+  total_fups(): Promise<number> {
+    return Promise.resolve(0);
+  }
+
+  enviou_todos(): Promise<boolean> {
+    return Promise.resolve(true);
+  }
+
+  abandonou_conversa(): Promise<boolean> {
+    return Promise.resolve(true);
+  }
+
+  agendar_proximo_fup(): Promise<boolean> {
+    return Promise.resolve(true);
+  }
+
+  fup_enviado(): Promise<void> {
+    return Promise.resolve();
+  }
+
+  resetar_fup(): Promise<void> {
+    return Promise.resolve();
+  }
+
+  conversa_finalizou(): Promise<boolean> {
+    return Promise.resolve(true);
+  }
+
+  enviar_em_conversa_finalizada(): Promise<boolean> {
+    return Promise.resolve(true);
+  }
+}
